ee30e36093aa7ce8ec8cb17f620f568a80a862b8
Pawel Wujczyk <pwujczyk@hotmail.com>
2024-02-07 07:10:25 +0100

    try to run

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import { PlateEditor } from './components/PlateEditor';
 
 function App() {
   return (
     <div className="App">
      hello
+     <PlateEditor></PlateEditor>
     </div>
   );
 }
--- src/components/PlateEditor.tsx
+++ src/components/PlateEditor.tsx
@@ -3,15 +3,15 @@
 import { createPlugins, Plate, RenderAfterEditable } from '@udecode/plate-common';
 import { createLinkPlugin, ELEMENT_LINK } from '@udecode/plate-link';
 
-import { LinkElement } from '@/components/plate-ui/link-element';
-import { LinkFloatingToolbar } from '@/components/plate-ui/link-floating-toolbar';
-import { Editor } from '@/components/plate-ui/editor';
-import { FixedToolbar } from '@/components/plate-ui/fixed-toolbar';
-import { FixedToolbarButtons } from '@/components/plate-ui/fixed-toolbar-buttons';
-import { FloatingToolbar } from '@/components/plate-ui/floating-toolbar';
-import { FloatingToolbarButtons } from '@/components/plate-ui/floating-toolbar-buttons';
-import { withPlaceholders } from '@/components/plate-ui/placeholder';
-import { TooltipProvider } from '@/components/plate-ui/tooltip';
+import { LinkElement } from './plate-ui/link-element';
+import { LinkFloatingToolbar } from './plate-ui/link-floating-toolbar';
+import { Editor } from './plate-ui/editor';
+import { FixedToolbar } from './plate-ui/fixed-toolbar';
+import { FixedToolbarButtons } from './plate-ui/fixed-toolbar-buttons';
+import { FloatingToolbar } from './plate-ui/floating-toolbar';
+import { FloatingToolbarButtons } from './plate-ui/floating-toolbar-buttons';
+import { withPlaceholders } from './plate-ui/placeholder';
+import { TooltipProvider } from './plate-ui/tooltip';
 
 const plugins = createPlugins(
   [
